365530c9a492bc333dcafd2b8faf2fdbaffd0002
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-22 04:27:05 -0700

    added all support function and printf

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN
+#define MAIN
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+int _putchar(char c);
+#endif
