15ff49ab4f1ec53a1bc58610b07ba86a374b8799
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-22 04:37:50 -0700

    unpush executable files and fix printf bug

--- printf.c
+++ printf.c
@@ -4,53 +4,49 @@
 * @format: char string to output
 * Return: count of char printed
 */
-int _printf(const char *format , ...)
+int _printf(const char *format, ...)
 {
-  va_list v1;
-
-  int i = 0, k = 0;
-  char buff[100]={0};
-  char * str;
-
-  va_start(v1, format);
-  while (format && format[i])
-    {
-      if (format[i] == '%')
-      {
-        i++;
-        switch(format[i])
-        {
-          case 'c' :
-            {
-              buff[k] = va_arg(v1, int);
-              k++;
-              break;
-            }
-
-          case 's':
-            {
-            str = va_arg(v1, char *);
-            _strcpy(&buff[k], str);
-            k += _strlen(str);
-            break;
-          }
-          case '%':
-            {
-              buff[k] = '%';
-              k++;
-            }
-        }
-      }
-      else
-      {
-        buff[k] = format[i];
-        k++;
-      }
-      i++;
-    }
-    write(1, &buff, k);
-    return k;
-  va_end(v1);
-
+va_list v1;
+int i = 0, k = 0;
+char buff[100] = {0};
+char *str;
+va_start(v1, format);
+while (format && format[i])
+{
+if (format[i] == '%')
+{
+i++;
+switch (format[i])
+{
+case 'c':
+{
+buff[k] = va_arg(v1, int);
+k++;
+break;
+}
+case 's':
+{
+str = va_arg(v1, char *);
+_strcpy(&buff[k], str);
+k += _strlen(str);
+break;
+}
+case '%':
+{
+buff[k] = '%';
+k++;
+}
+}
+}
+else
+{
+buff[k] = format[i];
+k++;
+}
+i++;
+}
+write(1, &buff, k);
+va_end(v1);
+return (k);
 }
 
