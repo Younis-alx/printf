e2aaaefc1a84c1f1ad8224f671204c1bc5b69651
ikky404 <jalil.ikram.jali@gmail.com>
2023-04-23 04:23:10 -0700

    indentation

--- printf.c
+++ printf.c
@@ -6,29 +6,29 @@
 */
 int _printf(const char *format, ...)
 {
-va_list v1;
-int i = 0, k = 0;
-int *kr = &k;
-char buff[BUFFSIZE];
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
-return (-1);
-va_start(v1, format);
-for (i = 0; format != NULL && format[i]; i++)
-{
-if (format[i] == '%')
-{
-i++;
-fmt_handler(kr, buff, format[i], v1);
-}
-else
-{
-buff[k] = format[i];
-k++;
-}
-}
-write(1, &buff, k);
-va_end(v1);
-return (k);
+	va_list v1;
+	int i = 0, k = 0;
+	int *kr = &k;
+	char buff[BUFFSIZE];
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
+		return (-1);
+	va_start(v1, format);
+	for (i = 0; format != NULL && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			fmt_handler(kr, buff, format[i], v1);
+		}
+		else
+		{
+			buff[k] = format[i];
+			k++;
+		}
+	}
+	write(1, &buff, k);
+	va_end(v1);
+	return (k);
 }
