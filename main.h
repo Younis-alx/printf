e43f6a33ae48d8cb787ac8c620c03ccccf332849
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-24 07:22:33 -0700

    updated the main.h

--- main.h
+++ main.h
@@ -17,22 +17,15 @@ typedef struct format_specs
 	int (*f)(va_list);
 } spec_t;
 
-/* List of prototypes and their respective declaration files */
 int _printf(const char *format, ...);
-
-/* 1-helper_functions.c */
 int _putchar(char c);
 int print_char(va_list ap);
 int print_str(va_list ap);
 int print_int(va_list ap);
 int print_decimal(va_list ap);
-/* 2-helper_functions.c */
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 int (*get_func(char s))(va_list);
 int print_percent(__attribute__((unused))va_list ap);
 int put_count(char c);
-/* 3-helper_functions.c */
-/* 4-helper_functions.c */
-
 #endif /* __MAIN_H__ */
