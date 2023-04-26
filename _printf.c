fa4e900829a9e1cf091ea8ec555e0168d9214895
ikky404 <jalil.ikram.jali@gmail.com>
2023-04-25 19:59:53 -0700

    re

--- _printf.c
+++ _printf.c
@@ -1,78 +1,56 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - print function
+ * @format: str
+ * Return: integer
  */
+
 int _printf(const char *format, ...)
 {
+	char *buff;
+	int  i = 0, count = 0;
+	va_list arg_value;
+	int (*func)(char *, int, va_list);
 
-    int i, printed, printed_chars;
-    int flags, width, precision, size, buff_ind = 0;
-    va_list list;
-    char buffer[BUFF_SIZE];
+	buff = malloc(4000);
+	if ((!format || !buff) || (format[0] == '%' && format[1] == '\0'))
+	{
+		free(buff);
+		exit(1);
+	}
 
-    printed = 0;
-    printed_chars = 0;
-    buff_ind = 0;
+	va_start(arg_value, format);
 
-    if (format == NULL)
-        return (-1);
-
-    va_start(list, format);
-
-    for (i = 0; format && format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            buffer[buff_ind++] = format[i];
-
-            if (buff_ind == BUFF_SIZE)
-
-                print_buffer(buffer, &buff_ind);
-
-            /* write(1, &format[i], 1);*/
-
-            printed_chars++;
-        }
-
-        else
-
-        {
-
-            print_buffer(buffer, &buff_ind);
-            flags = get_flags(format, &i);
-            width = get_width(format, &i, list);
-            precision = get_precision(format, &i, list);
-            size = get_size(format, &i);
-            ++i;
-
-            printed = handle_print(format, &i, list, buffer,flags, width, precision, size);
-
-            if (printed == -1)
-                return (-1);
-
-            printed_chars += printed;
-        }
-    }
-
-    print_buffer(buffer, &buff_ind);
-    va_end(list);
-
-    return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-
-void print_buffer(char buffer[], int *buff_ind)
-{
-    if (*buff_ind > 0)
-        write(1, &buffer[0], *buff_ind);
-
-    *buff_ind = 0;
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			buff[count] = format[i];
+			count++;
+		}
+		else
+		{
+			func = check_prtr(format[i + 1]);
+			if (!func)
+			{
+				buff[count] = '%';
+				i++;
+				count++;
+				continue;
+			}
+			count = func(&buff[count], count, arg_value);
+			i++;
+		}
+		i++;
+	}
+	write(1, buff, count);
+	va_end(arg_value);
+	free(buff);
+	return (count);
 }
