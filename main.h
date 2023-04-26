f78c26a6875eecb22471ec0b17d4f67d5c0938ec
ikky404 <jalil.ikram.jali@gmail.com>
2023-04-25 19:56:14 -0700

    edit

--- main.h
+++ main.h
@@ -1,52 +1,66 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <stdlib.h>
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <unistd.h>
 #include <stdarg.h>
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
+/* Declaration of typedef structs: */
+
 
 /**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
+* struct format_specs - format specifiers and connected function
+*
+* @c: such as those found in format string
+* @f: The function associated
+*/
+
+
+typedef struct ptr
 {
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-char *convert(unsigned long int num, int base, int lowercase);
+char *c;
+int (*f)(char *, int, va_list);
+} ptr_ch;
+
+
+
+
+
 int _printf(const char *format, ...);
-int (*get_print(char s))(va_list, flags_t *);
-int get_flag(char s, flags_t *f);
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
-int _putchar(char c);
-int _puts(char *str);
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-int print_address(va_list l, flags_t *f);
-int print_percent(va_list l, flags_t *f);
-#endif
+int (*check_prtr(char specifier))(char *, int, va_list);
+
+
+int func_d(char *buff, int count, va_list value);
+int func_i(char *buff, int count, va_list value);
+int func_u(char *buff, int count, va_list value);
+int func_o(char *buff, int count, va_list value);
+int func_x(char *buff, int count, va_list value);
+int func_X(char *buff, int count, va_list value);
+int func_c(char *buff, int count, va_list value);
+int func_s(char *buff, int count, va_list value);
+int func_ptg(char *buff, int count, va_list value);
+int func_b(char *buff, int count, va_list value);
+int func_r(char *buff, int count, va_list value);
+int func_R(char *buff, int count, va_list value);
+int func_p(char *buff, int count, va_list value);
+
+
+int _assign(char *buff, int count, char *value);
+int _rev_assign(char *buff, int count, char *value);
+
+
+int _strlen(char *s);
+char *_reverse(char *s, int n);
+char *_rot13(char *str);
+void _strcpy(char *dest, char *src);
+void _rev_ptr(char *str);
+
+
+int _abs(int n);
+char *_itoa(int value, int base);
+char *_utoa(unsigned int value, unsigned int base);
+char *_lcutoa(unsigned int value, unsigned int base);
+ #endif /* __MAIN_H__ */
+
